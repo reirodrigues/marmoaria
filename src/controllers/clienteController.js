7cf141f821c438d798e911f2a9a683ed046e3529
Sandro Garcia <sndr95@hotmail.com>
2022-08-29 21:44:58 -0300

    busca cep no front funcionando!

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -1,7 +1,7 @@
 const Cliente = require("../models/ClienteModel");
 
 exports.index = (req, res) => {
-  res.render("cliente", {
+  res.render("clientes/cliente", {
     cliente: {},
   });
 };
@@ -13,12 +13,12 @@ exports.register = async (req, res) => {
 
     if (cliente.errors.length > 0) {
       req.flash("errors", cliente.errors);
-      req.session.save(() => res.redirect("/cliente"));
+      req.session.save(() => res.redirect("clientes/cliente"));
       return;
     }
 
     req.flash("success", cliente.success);
-    req.session.save(() => res.redirect("/cliente/home"));
+    req.session.save(() => res.redirect("clientes/cliente/home"));
     return;
   } catch (e) {
     console.log(e);
@@ -33,7 +33,7 @@ exports.editIndex = async (req, res) => {
 
   if (!cliente) return res.render("404");
 
-  res.render("editarCliente", { cliente: cliente });
+  res.render("clientes/editarCliente", { cliente: cliente });
 };
 
 exports.edit = async (req, res) => {
@@ -44,15 +44,29 @@ exports.edit = async (req, res) => {
 
     if (cliente.errors.length > 0) {
       req.flash("errors", cliente.errors);
-      req.session.save(() => res.redirect(`/cliente/${req.params.id}`));
+      req.session.save(() => res.redirect(`clientes/cliente/${req.params.id}`));
       return;
     }
 
     req.flash("success", "Cliente Editado com sucesso");
-    req.session.save(() => res.redirect("/cliente/home"));
+    req.session.save(() => res.redirect("clientes/cliente/home"));
     return;
   } catch (e) {
     console.log(e);
     res.render("404");
+  }
+};
+exports.buscaCEP = async (req, res) => {
+  try {
+    const axios = require('axios').default;
+    const data = await axios.get(`https://viacep.com.br/ws/${req.body.cep}/json/`)
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+    return res.send(data);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ message: e.toString() });
   }
 };
